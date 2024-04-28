d5efda51fd5510bc03923b7277b83e574b7ecd92
tatsambon-tdc <tatsambonchristian@gmail.com>
2024-04-28 20:39:29 +0200

    UserAdmin fot Customized admin pages

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -44,5 +44,3 @@ class ModelTest(TestCase):
                                                          'test123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
-    
