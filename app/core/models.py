20c565bd643b6eab3623ee5e50f3377f79c73b23
tatsambon-tdc <tatsambonchristian@gmail.com>
2024-04-28 16:37:50 +0200

    Add superuser support

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
-from django.db import models  # noqa
+"""
+Database model
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManage(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create Save and Return a new user"""
+        if not email:
+            raise ValueError("User most have an Email addresse")
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        """Create super User"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        return user
+
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    """ User in the systeme"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=2500)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManage()
+
+    USERNAME_FIELD = 'email'
